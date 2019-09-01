895fdbd1a8d01be95c1c397f2737a8803c5f5985
woonzh <zhenhao1991@gmail.com>
2019-09-01 18:28:54 +0800

    t

--- test.py
+++ test.py
@@ -6,13 +6,13 @@ url='https://zhplaceholder.herokuapp.com/testWorker'
 #    'params': 10
 #        }
 
-#result=requests.get(url)
-#ans=json.loads(result.text)
-#jobid=ans['answer']['result']
+result=requests.get(url)
+ans=json.loads(result.text)
+jobid=ans['answer']['result']
 
-url='https://zhplaceholder.herokuapp.com/workerResult'
-params={
-    'jobId':jobid
-        }
-result=requests.get(url, params)
-print(result.text)
+#url='https://zhplaceholder.herokuapp.com/workerResult'
+#params={
+#    'jobId':jobid
+#        }
+#result=requests.get(url, params)
+#print(result.text)
