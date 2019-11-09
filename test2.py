4273a1822041666bf5f19bf495cc52f73f254751
woonzh <zhenhao1991@gmail.com>
2019-11-09 10:06:43 +0800

    t

--- test2.py
+++ test2.py
@@ -71,12 +71,12 @@ def defShiftAlloc():
 #                   sum(shiftAlloc[(i, j, k)] for i in dateList4 for j in nurses for k in aftSess)+\
 #                   sum(shiftAlloc[(i, j, k)] for i in dateList5 for j in nurses for k in aftSess)-\
 #                   (2*len(dateList3)))
-    aftAfterOffLst, aftAfterOffCount=noAftAfterOff(2)
-    model.Minimize(sum(aftAfterOffLst)-aftAfterOffCount)
-    consecAftLst, consecAftCount=noConsecutiveAftShit(purpose=2)
-    model.Minimize(sum(aftAfterOffLst)-aftAfterOffCount+sum(consecAftLst)-consecAftCount)
+#    aftAfterOffLst, aftAfterOffCount=noAftAfterOff(2)
+#    model.Minimize(sum(aftAfterOffLst)-aftAfterOffCount)
+#    consecAftLst, consecAftCount=noConsecutiveAftShit(purpose=2)
+#    model.Minimize(sum(aftAfterOffLst)-aftAfterOffCount+sum(consecAftLst)-consecAftCount)
             
-#    model.Maximize(sum(shiftAlloc[(i, j, k)] for i in dates for j in nurses for k in shifts))
+    model.Maximize(sum(shiftAlloc[(i, j, k)] for i in dates for j in nurses for k in shifts))
 
 def checkDate(dateVal, purpose=1):
     #check for holiday
@@ -151,7 +151,7 @@ def hoursPerWeek():
         for p in range(4):
             curDates=list(dates)[p*7:p*7+7]
 #            store=[calHours(x,y) for x in curDates for y in shifts]
-            model.Add(sum(shiftAlloc[(i,j,k)]*calHours(i,k) for i in curDates for k in shifts)==44)
+            model.Add(sum(shiftAlloc[(i,j,k)]*calHours(i,k) for i in curDates for k in shifts)>=44)
 
 def dayOff():
     for j in nurses:
