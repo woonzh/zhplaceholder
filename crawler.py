d283fd17050d6c2163641588ab80f883e6e33712
woonzh <zhenhao1991@gmail.com>
2020-04-26 15:14:10 +0800

    t

--- crawler.py
+++ crawler.py
@@ -265,7 +265,7 @@ class crawler:
             try:
                 if 'Key Data' in mod.text and cont==True:
                     self.actions.move_to_element(mod).perform()
-                    time.sleep(4)
+                    time.sleep(2)
                     cont=False
             except:
                 t=1
@@ -276,7 +276,7 @@ class crawler:
             print('navigate to key data success')
         
         self.driver.execute_script("window.scrollBy(0,500)")
-        time.sleep(3)
+        time.sleep(6)
         
         rows=self.driver.find_elements_by_xpath("""//tr[@class="summary-data__row"]""")
         count=0
@@ -287,7 +287,7 @@ class crawler:
                 store[headers[header]]=data
                 count+=1
         
-        print("%s data returned"%(str(count)))
+        print(store)
         
         return store
         
