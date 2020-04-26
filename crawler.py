ad128f1faafa21395a2483c67f0d44c17a9b150c
woonzh <zhenhao1991@gmail.com>
2020-04-26 15:56:54 +0800

    t

--- crawler.py
+++ crawler.py
@@ -79,7 +79,7 @@ class crawler:
             self.chrome_options.add_argument("--lang=en-us")
             self.chrome_options.add_argument('--disable-dev-shm-usage')
 #            self.chrome_options.add_argument("--start-maximized")
-            self.chrome_options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36")
+            self.chrome_options.add_argument("--user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36")
         
         self.startDriver()
             
