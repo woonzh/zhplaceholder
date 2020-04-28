7e30376570098fc8387740f0f2c7771e24afb18a
woonzh <zhenhao1991@gmail.com>
2020-04-28 12:21:03 +0800

    t

--- crawler.py
+++ crawler.py
@@ -73,7 +73,7 @@ class crawler:
             self.CHROMEDRIVER_PATH=os.environ.get('CHROMEDRIVER_PATH', None)
             self.useragentlist=['Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.75 Safari/537.36',\
                                 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36']
-            self.useragent=self.useragentlist[userAgentNum]
+            self.useragent=self.useragentlist[int(userAgentNum)]
             self.chrome_options = webdriver.ChromeOptions()
             self.chrome_options.binary_location = self.GOOGLE_CHROME_BIN
             self.chrome_options.add_argument('--headless')
